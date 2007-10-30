6d6ea8d8a490a8347552a202492d071e41827c21
mrj10@duke.edu <mrj10@duke.edu@360412fb-3b3c-0410-ad81-690bf4ca723d>
2007-10-30 00:19:34 +0000

    Changed to Java 1.5 instead of 1.6 so that we can run on OIT machines, and modified the READMe. Added prompting for Client name if necessary, and better error handling for incorrect port number passing on Server.

--- BattleshipServerProxy.java
+++ BattleshipServerProxy.java
@@ -32,36 +32,36 @@ public class BattleshipServerProxy implements Runnable, IBattleshipPlayer {
 		myModel.removePlayer(this);
 	}
 	
-	@Override //Don't need this
+	 //Don't need this
 	public boolean addMove(BattleshipMove move) {
 		return false;
 	}
 
-	@Override
+	
 	public void addShip(BattleshipPlacement bp) {
 		AddShip nm = new AddShip(bp);
 		myNMH.send(nm);
 	}
 
-	@Override //Don't need this
+	 //Don't need this
 	public boolean addShipPlacement(BattleshipPlacement place, ShipShape shape) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override //Don't need this
+	 //Don't need this
 	public void forfeit() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void gameIsOver(IBattleshipPlayer winner) {
 		GameOver nm = new GameOver(winner);
 		myNMH.send(nm);
 	}
 
-	@Override
+	
 	public BattleshipMove getMove() {
 		NeedMove nm = new NeedMove(null);
 		myNMH.send(nm);
@@ -69,7 +69,7 @@ public class BattleshipServerProxy implements Runnable, IBattleshipPlayer {
 		return new BattleshipMove(this, m.myCoordinate);
 	}
 
-	@Override
+	
 	public String getName() {
 		if(myPlayerName != null)
 			return myPlayerName;
@@ -80,7 +80,7 @@ public class BattleshipServerProxy implements Runnable, IBattleshipPlayer {
 		return myPlayerName;
 	}
 
-	@Override
+	
 	public BattleshipPlacement getShipPlacement(ShipShape shape) {
 		PlaceShip ps = new PlaceShip(null, shape);
 		myNMH.send(ps);
@@ -88,19 +88,19 @@ public class BattleshipServerProxy implements Runnable, IBattleshipPlayer {
 		return new BattleshipPlacement(this, p.myPoints);
 	}
 
-	@Override //Don't need this
+	 //Don't need this
 	public boolean isMoveValid(Coordinate move) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override //Don't need this
+	 //Don't need this
 	public boolean isPlacementValid(ArrayList<Coordinate> al, ShipShape ss) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+	
 	public void newGame() {
 		//System.err.println("ServerProxy "+this+" creating NewGame message.");
 		NewGame ng = new NewGame(null);
@@ -109,50 +109,50 @@ public class BattleshipServerProxy implements Runnable, IBattleshipPlayer {
 		//System.err.println("ServerProxy "+this+" sent NewGame message.");
 	}
 
-	@Override
+	
 	public void processMove(BattleshipMove m, CellState newState,
 			ShipShape shape) {
 		Move move = new Move(m, newState, shape);
 		myNMH.send(move);
 	}
 
-	@Override
+	
 	public void reset() {
 		Reset r = new Reset(null);
 		myNMH.send(r);
 	}
 
-	@Override //Don't need this
+	 //Don't need this
 	public void setModel(IBattleshipModel ibm) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override //Don't need this
+	 //Don't need this
 	public void setName(String name) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void setTurn(IBattleshipPlayer whoseTurn) {
 		SetTurn st = new SetTurn(whoseTurn);
 		myNMH.send(st);
 	}
 
-	@Override//Don't need this
+	//Don't need this
 	public void setView(IBattleshipView yourBattleView) {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
+	
 	public void shipSunk(BattleshipPlacement bp, ShipShape ss) {
 		ShipSunk shipsunk = new ShipSunk(bp, ss);
 		myNMH.send(shipsunk);
 	}
 
-	@Override
+	
 	public void updateScore(IBattleshipPlayer player, int newScore) {
 		ScoreUpdate su = new ScoreUpdate(player, newScore);
 		myNMH.send(su);
